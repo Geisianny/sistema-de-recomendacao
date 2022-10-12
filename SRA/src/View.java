0238c5e3a9f5c7697378d720e9f4b0e0e81d6bd1
Geisianny <geisiannybernardo@gmail.com>
2022-10-11 23:34:02 -0300

    implementacao do menu

--- SRA/src/View.java
+++ SRA/src/View.java
@@ -1,55 +1,88 @@
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class View {
 
-
     public void menu(Rede rede) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        Boolean continuar = true;
+        
+        while(continuar){
+            System.out.println("\n1 - Imprimir o mapa");
+            System.out.println("2 - Imprimir matriz");
+            System.out.println("3 - recomendar novos amigos");
+            System.out.println("4 - listar amigos");
+            System.out.println("0 - Sair");
 
+            int opcao = scan.nextInt();
 
-
-            while(true){
-                System.out.println("1 - Add vertices e aresta");
-                System.out.println("2 - Imprimir Grafo dado");
-                System.out.println("3 - Obter arvore de Kruskal");
-                System.out.println("4 - Obter caminho de Dijkstra");
-                System.out.println("5 - Obter arvore de Busca em Profundidade");
-                System.out.println("6 - Obter Fecho Transitivo do Grafo");
-                System.out.println("7 - Obter arvore de Busca em Largura");
-                System.out.println("8 - Obter Ordenacao Topologica");
-                System.out.println("9 - Obter Matriz do Algoritmo de Warshall");
-                System.out.println("0 - fim");
-
-                //opcao = Keyboard.readInt();
-
-                //dando um reset no grafo de resultado
-              //  resultado.clearLists();
-
-                //limpando verificadores booleanos
-               // inicial.limparArestaVisitada();
-               // inicial.limparVerticeVisitado();
-
-             //   switch (opcao){
-                 //   case 1:
-                      //      rede.imprimirMapa();
-                  //  case 2:
-                   //     recomendar
-                    //    break;
-
-                  //  default:
-                     //   System.out.println("invalido");
-                      //  break;
-              //  }
+            switch (opcao){
+                case 1:
+                    System.out.println("");
+                    rede.imprimirMapa();
+                    break;
+                case 2:
+                    System.out.println("");
+                    System.out.println("\n");
+                    rede.imprimir();
+                    System.out.println("\n");
+                    break;
+                case 3: 
+                    System.out.println("\nDigite o index da pessoa: ");
+                    int index = scan.nextInt();
+                    
+                    Pessoa pessoa = new Pessoa();
+                    
+                    List<Pessoa> pessoas = new ArrayList<>(); 
+                    pessoas = rede.getMapa();
+                    
+                    pessoa = pessoas.get(index);
+                    Recomendar(pessoa,rede);
+                    
+                    break;
+                
+                case 4:
+                    System.out.println("");
+                    Repositorio repositorio = new Repositorio();
+                    repositorio.bancoCom30pessoas();
+        
+                    ArrayList<Pessoa> list = repositorio.lista;
+                    
+                    for (int i=0;i< list.size();i++){
+                        System.out.println("Amigos de "+list.get(i).getNome()+
+                        "["+ list.indexOf(list.get(i)) +"]"+ ": ");
+                        System.out.println(rede.listarAmizades(list.get(i)));
+                        System.out.println("\n");
+                    }
+                    
+                case 0: 
+                    System.out.println("\nSaindo...");
+                    continuar = false;
+                    break;
+                    
+                default:
+                    
+                     System.out.println("\ninvalido");
+                     break;
 
             }
         }
-
-       public void Recomendar(Pessoa origem, Rede rede){
+    }
+      
+    
+    public void Recomendar(Pessoa origem, Rede rede){
 
         BuscaLargura b = new BuscaLargura(rede);
            b.execute(origem);
-           System.out.println("--------- Recomendações para " + origem.getNome() +" ------------\n");
+           System.out.println("--------- Recomendações para " + origem.getNome()
+                   +" ------------\n");
            for(int i=0; i<5;i++) {
-               System.out.println("| "+ b.recomendar.get(i).getNome()+" - " + ((b.recomendar.get(i).getPeso()) ) +"% de Paridade");
+               System.out.println("| "+ b.recomendar.get(i).getNome()+" - " 
+                       + ((b.recomendar.get(i).getPeso()) ) +"% de Paridade");
            }
        }
 

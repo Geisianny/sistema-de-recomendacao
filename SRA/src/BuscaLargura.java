868bcf74fe1d73af71872ca63888fc61c08ab605
Geisianny <geisiannybernardo@gmail.com>
2022-10-11 21:40:22 -0300

    Ajustando a busca em largura

--- SRA/src/BuscaLargura.java
+++ SRA/src/BuscaLargura.java
@@ -22,23 +22,41 @@ public class BuscaLargura {
     }
 
     public void inicializar() {
+       for (int i = 0; i < r.getNumeroVertices(); i++) {
+            this.cores[i] = Cor.BRANCO;
+            this.distancia[i] = -1;
+            this.pai[i] = -1;
+        }
+    }
+    public void execute(Pessoa p) {
+
+        int inicio = r.getIndiceVertice(p);
+        inicializar();
+        Queue<Integer> fila = new LinkedList<>();
+        this.cores[inicio] = Cor.CINZA;
         ordemAchados.add(r.getMapa().get(inicio).getNome());
         this.distancia[inicio] = 0;
         fila.add(inicio);
         int peso;
-        
-          while (fila.size() > 0) {
+
+        while (fila.size() > 0) {
             System.out.println(fila);
-            
-               this.distancia[u] = this.distancia[v] + 1;
+            int v = fila.poll();
+            List<Integer> adjacenciaV = r.listarAdjacencias(v);
+            for (int u : adjacenciaV) {
+                if (this.cores[u] == Cor.BRANCO) {
+                    this.pai[u] = v;
+                    this.distancia[u] = this.distancia[v] + 1;
                     this.cores[u] = Cor.CINZA;
                     ordemAchados.add(r.getMapa().get(u).getNome());
                     peso = CalcularPeso(inicio,u);
                     pesos.add(peso);
                     fila.add(u);
-           }
+                }
+            }
+        }
+
     }
-    
     public void imprimir() {
         System.out.println("Cores");
         for (Cor c : this.cores)
